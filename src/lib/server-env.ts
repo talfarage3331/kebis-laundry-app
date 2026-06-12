1a186db1e4afc39c912781ac3a86febbc31a6523
Tal Farage <talfarage3331@gmail.com>
2026-06-12 16:25:34 +0300

    fix(push): format private key PEM string to avoid invalid characters from escaped backslash newlines

--- src/lib/server-env.ts
+++ src/lib/server-env.ts
@@ -9,7 +9,34 @@ export const SERVICE_ACCOUNT_FALLBACK = {
   type: "service_account",
   project_id: "kevisa-5983b",
   private_key_id: "3881862ca1c2fa250e89a8b5e9344e8437567aca",
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpo/vfEufvPROf\nWKlB8pKxDtLNYXMvCiiDr3GPDlHS9Cp9ad/UcEH9Czmc53ka7CpkHwfp77T9q/Vs\nz/29P3jP5bG4WGm8b8T9GWjcWnPUwooum0bQCGtPrOewTDj9DwBQIHN3TBO8+Bwq\n9X84wvsrveYBDU5OC25KTkOIneUNOKjjdod/xddThbLeC/6CxAmLl5LWKcHpm/Tu\n4jwvINecn+U1COW8FWygvEWtLb4ZuAJmh4HuXSsUHSsE+yzdGVpQi8DYzyErtiLd\nw2pPb5aSKzi4tmKxtNsqA84cPkX8udWRymbLkOOsi8aErPemQ6bvr68Htj4ciYws\nIVd5OMi9AgMBAAECggEACG7aYZrtFI+cIbF/YS53xt1WXYyGNJTZcem/PYM9eOfC\nPfpM/MtOKHq4n47CjolQy9SJaO0i5Ee6W1v/6zq+x3z4Z6eI2BZ+poNZxbcpvMOl\nVA723KFyg43e9adltNocTp9nsLWY4e276WcgrtfDMzKNPsj5zHeVK79LbFJHFZqs\n/Uil7MnFe1bOFqOcQzxiOJiv5iLICFM+zJARc4SBtOz//eNdRtXeb+SxKIOGAwJA\nwtxt5+M9LpwvYvzdnMxw1AqQDNsTtSPSM0iwG7w9bcJF+L44cVHoVpStU5r0u3E5\nSs5cBrM15XNvLG5xPEkRh5Ar2xLMRYObzeWUNBvxaQKBgQDTa1UBlLkwGYJmvWMM\n1QTRa400IVifkyrrlR2a2Hp08mrrHZkvn5AuK/MrvTdegLq0J9WoHhw4lZPEv5iv\nT5qPBsXY/gpxJrHrLu9V0SXwdWZ7F6ZJJ5R68hMjTrP1DsyrRlLLhO8tefCfp1bE\puQ+5V3vuhpUZPZk2BV7mPvzdQKBgQDNaWMFXq5P+8NkoplT8HKxwjZz1NsgYMCx\nC1TnkTTyfmjlq4wXWU8Cqf+wb99wlmiJX08n4BNkj9WTC/vC/v1/Vmto2l85vINZ\bi5GtegRipxphf2m8ZetY6/mcN/1hnMOdnoF3K8CjlvmZT9iq3rloUwg+CFnFUgg\RoiX4oE/KQKBgFb37QT0d+VoaPL4bxllO1Ema/SIzxr9gAde1MnQyTb4TbEgANbN\nTZMgsyxH7tHqJGNdi49Xq4Y/SYUCx70+ArdZMuPbRNJc941mRj/IBFJRCPITvWyU\nYojcsbCxfGePSfQevopHydesNKaIQLIucO/KjITDynby/URoexXXzbTNAoGAPpUQ\LS+mjgnXgw6jBlboqs3QB0RfqqKvsoEQUikEZ6kpzmeQnACCuP4QP7Il8khw+wGF\nvVFgty+3U0DAaK2FkElFGkIF5zJPFm0iPjrnxIhllSTUngtXOeV4Tw4uvcBDx7FD\nBgwxaebziuNoB2jVXabh6d2PV7e9LNAwh43j8NkCgYEAzCxlw6OelIWUa1HzZkvm\neQScFbpz69iolOTfQJeSMQDBwGNNfRRCzqjABtN+V5ESKzwE7O06IhOR2foww2ju\nvqoWLdWv+Zvz0DTLb54MUtdFYh3QRp1kZ1tudDHjTuz7Ois7LJepMvRCVtkN92Fl\n+nfxSf9Ns1YpbmEaDE+Km3g=\n-----END PRIVATE KEY-----\n".replace(/\\n/g, "\n"),
+  private_key: ("-----BEGIN PRIVATE KEY-----\n" +
+    "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCpo/vfEufvPROf\n" +
+    "WKlB8pKxDtLNYXMvCiiDr3GPDlHS9Cp9ad/UcEH9Czmc53ka7CpkHwfp77T9q/Vs\n" +
+    "z/29P3jP5bG4WGm8b8T9GWjcWnPUwooum0bQCGtPrOewTDj9DwBQIHN3TBO8+Bwq\n" +
+    "9X84wvsrveYBDU5OC25KTkOIneUNOKjjdod/xddThbLeC/6CxAmLl5LWKcHpm/Tu\n" +
+    "4jwvINecn+U1COW8FWygvEWtLb4ZuAJmh4HuXSsUHSsE+yzdGVpQi8DYzyErtiLd\n" +
+    "w2pPb5aSKzi4tmKxtNsqA84cPkX8udWRymbLkOOsi8aErPemQ6bvr68Htj4ciYws\n" +
+    "IVd5OMi9AgMBAAECggEACG7aYZrtFI+cIbF/YS53xt1WXYyGNJTZcem/PYM9eOfC\n" +
+    "PfpM/MtOKHq4n47CjolQy9SJaO0i5Ee6W1v/6zq+x3z4Z6eI2BZ+poNZxbcpvMOl\n" +
+    "VA723KFyg43e9adltNocTp9nsLWY4e276WcgrtfDMzKNPsj5zHeVK79LbFJHFZqs\n" +
+    "/Uil7MnFe1bOFqOcQzxiOJiv5iLICFM+zJARc4SBtOz//eNdRtXeb+SxKIOGAwJA\n" +
+    "wtxt5+M9LpwvYvzdnMxw1AqQDNsTtSPSM0iwG7w9bcJF+L44cVHoVpStU5r0u3E5\n" +
+    "Ss5cBrM15XNvLG5xPEkRh5Ar2xLMRYObzeWUNBvxaQKBgQDTa1UBlLkwGYJmvWMM\n" +
+    "1QTRa400IVifkyrrlR2a2Hp08mrrHZkvn5AuK/MrvTdegLq0J9WoHhw4lZPEv5iv\n" +
+    "T5qPBsXY/gpxJrHrLu9V0SXwdWZ7F6ZJJ5R68hMjTrP1DsyrRlLLhO8tefCfp1bE\n" +
+    "puQ+5V3vuhpUZPZk2BV7mPvzdQKBgQDNaWMFXq5P+8NkoplT8HKxwjZz1NsgYMCx\n" +
+    "C1TnkTTyfmjlq4wXWU8Cqf+wb99wlmiJX08n4BNkj9WTC/vC/v1/Vmto2l85vINZ\n" +
+    "bi5GtegRipxphf2m8ZetY6/mcN/1hnMOdnoF3K8CjlvmZT9iq3rloUwg+CFnFUgg\n" +
+    "RoiX4oE/KQKBgFb37QT0d+VoaPL4bxllO1Ema/SIzxr9gAde1MnQyTb4TbEgANbN\n" +
+    "TZMgsyxH7tHqJGNdi49Xq4Y/SYUCx70+ArdZMuPbRNJc941mRj/IBFJRCPITvWyU\n" +
+    "YojcsbCxfGePSfQevopHydesNKaIQLIucO/KjITDynby/URoexXXzbTNAoGAPpUQ\n" +
+    "LS+mjgnXgw6jBlboqs3QB0RfqqKvsoEQUikEZ6kpzmeQnACCuP4QP7Il8khw+wGF\n" +
+    "vVFgty+3U0DAaK2FkElFGkIF5zJPFm0iPjrnxIhllSTUngtXOeV4Tw4uvcBDx7FD\n" +
+    "BgwxaebziuNoB2jVXabh6d2PV7e9LNAwh43j8NkCgYEAzCxlw6OelIWUa1HzZkvm\n" +
+    "eQScFbpz69iolOTfQJeSMQDBwGNNfRRCzqjABtN+V5ESKzwE7O06IhOR2foww2ju\n" +
+    "vqoWLdWv+Zvz0DTLb54MUtdFYh3QRp1kZ1tudDHjTuz7Ois7LJepMvRCVtkN92Fl\n" +
+    "+nfxSf9Ns1YpbmEaDE+Km3g=\n" +
+    "-----END PRIVATE KEY-----\n").replace(/\\n/g, "\n"),
   client_email: "firebase-adminsdk-fbsvc@kevisa-5983b.iam.gserviceaccount.com",
   client_id: "101947721606480397342",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
